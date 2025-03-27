80d7c9b2e09dc4daad88021b6e0c90186b0631b5
prabal <imprabalpratap@gmail.com>
2025-03-26 18:57:22 -0700

    Adding Project files, tailwind & css setup, updated assets and metadata

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="h1-bold font-space-grotesk">Welcome to next</h1>
+      <h1 className="h1-bold font-inter">Welcome to next</h1>
+      <h1 className="h1-bold">Welcome to next</h1>
+    </>
+  );
+}
