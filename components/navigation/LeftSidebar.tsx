3214a4dd9b260202d599e22f17480b7344b6ae27
prabal <imprabalpratap@gmail.com>
2025-03-31 15:33:53 -0700

    Implemented Local Search Bar

--- components/navigation/LeftSidebar.tsx
+++ components/navigation/LeftSidebar.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 const LeftSidebar = () => {
   return (
-    <section className="custom-scrollbar backgound-light900_dark200 light-border sticky left-0 top-0 h-screen flex flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+    <section className="custom-scrollbar backgound-light900_dark200 light-border sticky left-0 top-0 h-screen flex flex-col justify-between overflow-y-auto border-r p-6 pt-28 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-col flex-1 gap-6">
         <NavLinks />
       </div>
--- constants/route.ts
+++ constants/route.ts
@@ -4,5 +4,6 @@ const ROUTES = {
   SIGN_UP: "/sign-up",
   PROFILE: (id: string) => `/profile/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
+  ASK_QUESTION: "/ask-question",
 };
 export default ROUTES;
