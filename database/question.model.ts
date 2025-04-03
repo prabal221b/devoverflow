6d353f42cb67d0964b7daa6d7e183f2bfcb09b18
prabal <imprabalpratap@gmail.com>
2025-04-02 20:03:36 -0700

    updates model to export interfaces as suggesed in mongoose documentation

--- database/question.model.ts
+++ database/question.model.ts
@@ -1,4 +1,4 @@
-import { model, models, Schema, Types } from "mongoose";
+import { model, models, Schema, Types, Document } from "mongoose";
 
 export interface IQuestion {
   title: string;
@@ -10,6 +10,7 @@ export interface IQuestion {
   upvotes: number;
   downvotes: number;
 }
+export interface IQuestionDoc extends IQuestion, Document {}
 
 const QuestionSchema = new Schema<IQuestion>(
   {
