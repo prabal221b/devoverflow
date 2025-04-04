e013264256649e16697cf027a3f9d215ac69be3c
prabal <imprabalpratap@gmail.com>
2025-04-04 13:53:26 -0700

    Implement CREATE and READ user route

---
+++ types/global.d.ts
@@ -0,0 +1,36 @@
+interface Question {
+  _id: string;
+  title: string;
+  tags: Tag[];
+  author: Author;
+  createdAt: Date;
+  upvotes: number;
+  answers: number;
+  views: number;
+}
+interface Tag {
+  _id: string;
+  name: string;
+}
+
+interface Author {
+  _id: string;
+  name: string;
+  image: string;
+}
+
+type ActionResponse<T = null> = {
+  success: boolean;
+  data?: T;
+  error?: {
+    message: string;
+    details?: Record<string, string[]>;
+  };
+  status?: number;
+};
+
+type SuccessResponse<T = null> = ActionResponse<T> & { success: true };
+type APIResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>;
+
+type ErrorResponse = ActionResponse<undefined> & { success: false };
+type APIErrorResponse = NextResponse<ErrorResponse>;
