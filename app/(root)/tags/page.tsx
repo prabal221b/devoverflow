20e91a89f134759e79e68cef32c5064d0a466b8c
prabal <imprabalpratap@gmail.com>
2025-04-07 17:52:23 -0700

    getTags Server Action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { getTags } from "@/lib/actions/tag.actions";
 
-const Tags = () => {
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "query",
+  });
+
+  const { tags } = data || {};
+
+  console.log("TAGS", JSON.stringify(tags, null, 2));
+
   return <div>Tags</div>;
 };
 
