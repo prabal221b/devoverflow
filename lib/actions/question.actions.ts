d7fb4524162bd06c7672a9162cf74601311298ae
prabal <imprabalpratap@gmail.com>
2025-04-10 01:19:29 -0700

    Implemented Question Details View Increment

--- lib/actions/question.actions.ts
+++ lib/actions/question.actions.ts
@@ -7,6 +7,7 @@ import {
   AskQuestionSchema,
   EditQuestionSchema,
   GetQuestionSchema,
+  IncrementViewsSchema,
   PaginatedSearchParamsSchema,
 } from "../validations";
 import Question from "@/database/question.model";
@@ -193,7 +194,7 @@ export async function getQuestion(
   const validationResult = await action({
     params,
     schema: GetQuestionSchema,
-    authorize: true,
+    authorize: false,
   });
 
   if (validationResult instanceof Error) {
@@ -277,6 +278,38 @@ export async function getQuestions(
     return {
       success: true,
       data: { questions: JSON.parse(JSON.stringify(questions)), isNext },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function incrementViews(
+  params: IncrementViewsParams
+): Promise<ActionResponse<{ views: number }>> {
+  const validationResult = await action({
+    params,
+    schema: IncrementViewsSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { questionId } = validationResult.params!;
+
+  try {
+    const question = await Question.findById(questionId);
+
+    if (!question) throw new Error("Question not found");
+
+    question.views += 1;
+
+    await question.save();
+
+    return {
+      success: true,
+      data: { views: question.views },
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
