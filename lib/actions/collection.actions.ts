67b560aecf84b6c1811a861391ccb2ab749331ad
prabal <imprabalpratap@gmail.com>
2025-04-12 00:22:34 -0700

    Implement add to collection action

---
+++ lib/actions/collection.actions.ts
@@ -0,0 +1,64 @@
+"use server";
+
+import { Collection, Question } from "@/database";
+import action from "../handlers/action";
+import handleError from "../handlers/error";
+import { CollectionBaseSchema } from "../validations";
+import { revalidatePath } from "next/cache";
+import ROUTES from "@/constants/routes";
+
+export async function toggleSaveQuestion(
+  params: CollectionBaseParams
+): Promise<ActionResponse<{ saved: boolean }>> {
+  const validatedResult = await action({
+    params,
+    schema: CollectionBaseSchema,
+    authorize: true,
+  });
+
+  if (validatedResult instanceof Error) {
+    return handleError(validatedResult) as ErrorResponse;
+  }
+
+  const { questionId } = validatedResult.params!;
+  const userId = validatedResult.session?.user?.id;
+
+  try {
+    const question = await Question.findById(questionId);
+
+    if (!question) {
+      throw new Error("Question not found");
+    }
+
+    const collection = await Collection.findOne({
+      question: questionId,
+      author: userId,
+    });
+
+    if (collection) {
+      await Collection.findByIdAndDelete(collection.id);
+      return {
+        success: true,
+        data: {
+          saved: false,
+        },
+      };
+    }
+
+    await Collection.create({
+      question: questionId,
+      author: userId,
+    });
+
+    revalidatePath(ROUTES.QUESTION(questionId));
+
+    return {
+      success: true,
+      data: {
+        saved: true,
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
--- types/action.d.ts
+++ types/action.d.ts
@@ -63,3 +63,7 @@ interface HasVotedResponse {
   hasUpVoted: boolean;
   hasDownVoted: boolean;
 }
+
+interface CollectionBaseParams {
+  questionId: string;
+}
