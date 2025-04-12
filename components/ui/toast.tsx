4404ee62f0cc93d02cb4a4af697991fe92534df4
prabal <imprabalpratap@gmail.com>
2025-04-12 01:53:15 -0700

    updated toast from shadcn

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -3,9 +3,8 @@
 import * as React from "react";
 import * as ToastPrimitives from "@radix-ui/react-toast";
 import { cva, type VariantProps } from "class-variance-authority";
-import { X } from "lucide-react";
-
 import { cn } from "@/lib/utils";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
 const ToastProvider = ToastPrimitives.Provider;
 
@@ -83,7 +82,7 @@ const ToastClose = React.forwardRef<
     toast-close=""
     {...props}
   >
-    <X className="h-4 w-4" />
+    <Cross2Icon className="h-4 w-4" />
   </ToastPrimitives.Close>
 ));
 ToastClose.displayName = ToastPrimitives.Close.displayName;
