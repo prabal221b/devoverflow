e0ff5bf65995b67d498124ce1d06d9dff60bf78e
prabal <imprabalpratap@gmail.com>
2025-04-17 23:41:40 -0700

    Integrated Top Questions on Right sidebar

--- components/navigation/RightSidebar.tsx
+++ components/navigation/RightSidebar.tsx
@@ -3,14 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import TagCards from "../cards/TagCards";
-
-const hotQuestions = [
-  { _id: "1", title: "How to create a custom hook in React?" },
-  { _id: "2", title: "How to use React Query?" },
-  { _id: "3", title: "How to use Redux?" },
-  { _id: "4", title: "How to use React Router?" },
-  { _id: "5", title: "How to use React Context?" },
-];
+import { getHotQuestions } from "@/lib/actions/question.actions";
+import DataRenderer from "../DataRenderer";
 
 const popularTags = [
   { _id: "1", name: "react", questions: 100 },
@@ -20,31 +14,45 @@ const popularTags = [
   { _id: "5", name: "react-query", questions: 75 },
 ];
 
-const RightSidebar = () => {
+const RightSidebar = async () => {
+  const { success, data: hotQuestions, error } = await getHotQuestions();
   return (
     <section className="pt-36 custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
 
-        <div className="mt-7 flex w-full flex-col gap-[30px]">
-          {hotQuestions.map(({ _id, title }) => (
-            <Link
-              key={_id}
-              href={ROUTES.PROFILE(_id)}
-              className="flex cursor-pointer justify-between gap-7"
-            >
-              <p className="body-medium text-dark500_light700">{title}</p>
+        <DataRenderer
+          data={hotQuestions}
+          empty={{
+            title: "No Questions Found",
+            message: "No questions have been asked yet",
+          }}
+          success={success}
+          error={error}
+          render={(hotQuestions) => (
+            <div className="mt-7 flex w-full flex-col gap-[30px]">
+              {hotQuestions.map(({ _id, title }) => (
+                <Link
+                  key={_id}
+                  href={ROUTES.QUESTION(_id)}
+                  className="flex cursor-pointer justify-between gap-7"
+                >
+                  <p className="body-medium text-dark500_light700 line-clamp-1">
+                    {title}
+                  </p>
 
-              <Image
-                src="/icons/chevron-right.svg"
-                alt="Chevron"
-                width={23}
-                height={23}
-                className="invert-colors"
-              />
-            </Link>
-          ))}
-        </div>
+                  <Image
+                    src="/icons/chevron-right.svg"
+                    alt="Chevron"
+                    width={23}
+                    height={23}
+                    className="invert-colors"
+                  />
+                </Link>
+              ))}
+            </div>
+          )}
+        />
       </div>
 
       <div className="mt-16">
