71085d61a99ea600786e7ed92f6baa87a020da2e
prabal <imprabalpratap@gmail.com>
2025-04-22 01:29:25 -0700

    added cache for getQuestion as it is being called multiple times on the same page

--- lib/actions/question.actions.ts
+++ lib/actions/question.actions.ts
@@ -17,6 +17,7 @@ import TagQuestion from "@/database/tag-question.model";
 import dbConnect from "../mongoose";
 import { Answer, Collection, Vote } from "@/database";
 import { revalidatePath } from "next/cache";
+import { cache } from "react";
 
 export async function createQuestion(
   params: CreateQuestionParams
@@ -192,7 +193,7 @@ export async function editQuestion(
   }
 }
 
-export async function getQuestion(
+export const getQuestion = cache(async function getQuestion(
   params: GetQuestionParams
 ): Promise<ActionResponse<Question>> {
   const validationResult = await action({
@@ -217,7 +218,7 @@ export async function getQuestion(
   } catch (error) {
     return handleError(error) as ErrorResponse;
   }
-}
+});
 
 export async function getQuestions(
   params: PaginatedSearchParams
