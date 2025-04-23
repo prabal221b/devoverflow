de5ed3188bc445d98f4bf7e2f0aa26d79724edac
prabal <imprabalpratap@gmail.com>
2025-04-23 02:53:39 -0700

    fixed margin for better visibility of "stats" header

--- components/user/Stats.tsx
+++ components/user/Stats.tsx
@@ -28,7 +28,7 @@ const Stats = ({ totalAnswers, totalQuestions, badges }: Props) => {
     <div className="mt-3">
       <h4 className="h3-semibold text-dark200_light900">Stats</h4>
 
-      <div className="mt-5 grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-4">
+      <div className="mt-3 grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-4">
         <div className="light-border background-light900_dark300 flex flex-wrap items-center justify-start gap-4 rounded-md border p-6 shadow-light300 dark:shadow-dark-200">
           <div>
             <p className="paragraph-semibold text-dark200_light900">
